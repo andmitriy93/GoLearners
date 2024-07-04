ab7bbfa0e918b53cb065465445da169c67e809de
dmitrii <an.dmitriy@gmail.com>
2024-07-04 10:35:19 -0500

    Exercise of pointers

--- Pointers/74_exercise_pointer/main.go
+++ Pointers/74_exercise_pointer/main.go
@@ -2,8 +2,31 @@ package main
 
 import "fmt"
 
+var (
+	a, b, c *string
+	d  *int
+)
+
+func init() {
+	p := "Drop by drop, the bucket gets filled."
+	q := "Persistently, patiently you are bound to succeed."
+	r := "The meaning of life is ..."
+	n := 42
+	a = &p
+	b = &q
+	c = &r
+	d = &n
+}
 
 func main() {
-	x := 42
-	fmt.Println(&x)
+	fmt.Printf("a value %v\t and type %T\n", a, a)
+	fmt.Printf("b value %v\t and type %T\n", b, b)
+	fmt.Printf("c value %v\t and type %T\n", c, c)
+	fmt.Printf("d value %v\t and type %T\n", d, d)
+
+	fmt.Println(*a)
+	fmt.Println(*b)
+	fmt.Println(*c)
+	fmt.Println(*d)
 }
+
