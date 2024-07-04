4e2f1b9c415c7dd6cb7e503bd21dcbc18901200d
dmitrii <an.dmitriy@gmail.com>
2024-07-03 21:10:39 -0500

    interface method sets

--- Pointers/method_sets/main.go
+++ Pointers/method_sets/main.go
@@ -15,12 +15,23 @@ func (d *dog) run() {
 	fmt.Println("My name is", d.first, "and, I'm running")
 }
 
+type youngin interface {
+	walk()
+	run()
+}
+
+func youngRun(y youngin) {
+	y.run()
+}
+
 func main() {
 	d1 := dog{"Henry"}
 	d1.walk()
 	d1.run()
+	// youngRun(d1)
 
 	d2 := &dog{"Padget"}
 	d2.walk()
 	d2.run()
+	youngRun(d2)
 }
